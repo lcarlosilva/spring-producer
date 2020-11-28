ac0df527a1cc677e4e4e5df7025b09c679479d0b
Luiz Carlos <luiz.silva@luizalabs.com>
2020-11-28 00:11:02 -0300

    remocao de comentário

--- src/main/java/com/luiz/udemy/spring/producer/dto/Message.java
+++ src/main/java/com/luiz/udemy/spring/producer/dto/Message.java
@@ -14,13 +14,4 @@ public class Message implements Serializable {
 
     private String text;
 
-    /*public Message() {}
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }*/
 }
